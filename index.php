ee1ad17be8ca5264474c03a5f5f821b86944be13
Oscar Gomes <medinagomes.oscar7@gmail.com>
2021-04-08 19:23:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <title>The life snakes/title>
+    <title>The life snakes</title>
 <body>
 <h1>The life ever snake brô</h1>
 <img src="6vcukPLvH4mx3HmxZ1gkHffCccXouyzKk2IZZO11BOA.jpg" alt="meme" style="width:300px;height:500px;">
