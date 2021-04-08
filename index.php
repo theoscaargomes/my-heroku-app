72a72d82c3acf57ed9adb203ac7674e2816b8648
Oscar Gomes <medinagomes.oscar7@gmail.com>
2021-04-08 19:00:39 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <title>The life snakes/title>
+<body>
+<h1>The life ever snake brô</h1>
+<img src="IMG_20190724_125434046.jpg" alt="meme" style="width:300px;height:500px;">
+</body>
+</html>
